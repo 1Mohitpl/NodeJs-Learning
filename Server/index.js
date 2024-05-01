6610a9711b146266d9cd9caa63c70f5c31cec6aa
Mohit kumar paul <mohit1paul@gmail.com>
2024-05-02 01:52:39 +0530

    server

--- Server/index.js
+++ Server/index.js
@@ -1,8 +1,11 @@
 const http = require ("http");
 const fs = require ("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) => {
     const log = `${Date.now()} : ${req.url} New request recived\n `;
+    const myUrl = url.parse(req.url);
+    console.log(myUrl);
       fs.appendFile("log.txt",log, (err, data)=>{
           switch(req.url) {
 
