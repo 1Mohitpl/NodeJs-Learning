c5fe0fee7381c3af1dca62bf172f4812c37a62eb
Mohit kumar paul <mohit1paul@gmail.com>
2024-05-07 02:14:27 +0530

    url handling

--- New_server/index.js
+++ New_server/index.js
@@ -1,17 +1,35 @@
 const http = require("http");
 const fs = require("fs");
-const { time } = require("console");
+const url = require("url");
 
 const newServer = http.createServer((req, res) => {
      
-    const log = `${Date.now()} : new req aries\n`;
+    const log = `${Date.now()} :${req.url} New request recived\n`;
+    const carUrl = url.parse(req.url);   // used to return the object of the path 
+    console.log(carUrl);
 
     fs.appendFile("log.txt", log, (err, data) =>{
-        res.end(time.now);
+        switch (req.url){
+            case '/food':
+                res.end("here your order....")
+            break;
+            case '/price':
+                res.end("price of the food is....")
+                break;
+            case '/about' :
+                res.end('the food is vegtable')
+            break;
+
+            default :
+            res.end("404 not found");
+
+        }
+
+
 
     });
 
    
 });
 
-newServer.listen(3737, () => console.log("server is started"));
+newServer.listen(8000, () => console.log("server is started"));
