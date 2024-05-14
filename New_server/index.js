0cabf8bfe2f5b5ad9a53bf8ed3d81913616012f8
Mohit kumar paul <mohit1paul@gmail.com>
2024-05-14 19:01:23 +0530

    git commit

--- New_server/index.js
+++ New_server/index.js
@@ -32,4 +32,4 @@ const newServer = http.createServer((req, res) => {
    
 });
 
-newServer.listen(2000, () => console.log("server is started"));
+newServer.listen(7000, () => console.log("server is started"));
