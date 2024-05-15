987d810dba99e37b31dafac1b4df212606e8df8f
Mohit kumar paul <mohit1paul@gmail.com>
2024-05-15 14:11:39 +0530

    add expressjs

--- Server/index.js
+++ Server/index.js
@@ -1,34 +1,47 @@
 const http = require ("http");
-const fs = require ("fs");
-const url = require("url");
+// const fs = require ("fs");
+// const url = require("url");
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()} : ${req.url} New request recived\n `;
-    const myUrl = url.parse(req.url);
-    console.log(myUrl);
-      fs.appendFile("log.txt",log, (err, data)=>{
-          switch(req.url) {
+const app = express();
 
-            case '/':   // root path or Home page
-            res.end("hello from ! whelcome to home page");
-            break;
-            case '/carriers' :
-            res.end("we are hiring technical consultant...");
-             
-           break;
-           case '/about' :
-            res.end(" hey, this is hiring manager mohit !...");
-            break;
-
-            default :
-            res.end("404 not found")
-          }
-
-      });
-
-
- 
+app.get("/", (req, res) =>{
+    return res.send("hello from home page");
 });
+
+app.get("/about", (Req, res) => {
+  return res.send("hello from about page");
+});
+
+
+
+  //                                                    function myHandler(req, res) {
+  // const log = `${Date.now()} : ${req.url} New request recived\n `;
+  //   const myUrl = url.parse(req.url);
+  //   console.log(myUrl);
+  //     fs.appendFile("log.txt",log, (err, data)=>{
+  //         switch(req.url) {
+
+  //           case '/':   // root path or Home page
+  //           res.end("hello from ! whelcome to home page");
+  //           break;
+  //           case '/carriers' :
+  //           res.end("we are hiring technical consultant...");
+             
+  //          break;
+  //          case '/about' :
+  //           res.end(" hey, this is hiring manager mohit !...");
+  //           break;
+
+  //           default :
+  //           res.end("404 not found")
+  //         }
+
+  //     });
+
+  //                                                     }
+
+const myServer = http.createServer(app);
 
 myServer.listen(4001, () => {
 
