f9f994a7ab41a1947f30d2524043e87dbc5a3c4e
Mohit kumar paul <mohit1paul@gmail.com>
2024-05-25 20:08:19 +0530

    sync and async funtion

--- file.js
+++ file.js
@@ -1,6 +1,16 @@
+// const { error } = require("console");
+// const fs = require("fs");
+
+// // fs.writeFileSync("./main.txt", "create an file");  // sync... blocking request 
+
+// fs.writeFile("./test.tex", "async function", (err) => {});   // asyn... Non-Blocking request
+
 const { error } = require("console");
 const fs = require("fs");
 
-// fs.writeFileSync("./main.txt", "create an file");  // sync  create file 
 
-fs.writeFile("./test.tex", "async function", (err) => {});   // asyn function 
+// //sycn...
+// fs.writeFileSync("./test.txt", "hey there")
+
+//asycn...
+fs.writeFile("./text.text", "hey world is supper",(err) => {});
