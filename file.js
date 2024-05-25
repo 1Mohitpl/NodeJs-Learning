5b0ca4a635946137c9899d060eed5c38fe21aae1
Mohit kumar paul <mohit1paul@gmail.com>
2024-05-25 20:48:38 +0530

    file reading

--- file.js
+++ file.js
@@ -3,4 +3,15 @@ const fs = require("fs");
 
 // fs.writeFileSync("./main.txt", "create an file");  // sync  create file 
 
-fs.writeFile("./test.tex", "async function", (err) => {});   // asyn function 
+// fs.writeFile("./test.tex", "async function", (err) => {});   // asyn function 
+
+
+ fs.readFile("./contacts,txt", "utf-8", (err, result) => {
+ 
+    if(err){                                                    // async function never return any value, it alwayes ask call back function;
+                                                                // here read the text file
+        console.log("Error", err)
+    }else{
+        console.log(result)
+    }
+});
